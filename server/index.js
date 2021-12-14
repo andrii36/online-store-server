6b760135cf1f0df1f8080fe9dec5294e6be07b8d
unknown <vovchenkoandrii@gmail.com>
2021-12-13 23:31:09 -0800

    server architecture

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const router = require('./router/index')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use('/api', router)
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log("Running on " + PORT))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
